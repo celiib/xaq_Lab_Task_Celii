8bb77e90ba228c2d3bb1c9c085fccefb977bd0a3
Brendan Celii <celiib@northampton.k12.nc.us>
2018-08-08 04:25:43 -0500

    fixing typo on setup

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     packages=['sfa'],
     install_requires=['torch','numpy','math','matplotlib'], #external packages as dependencies
     url = 'https://github.com/sheriferson/simplestatistics',
-    download_url = 'https://github.com/sheriferson/simplestatistics/tarball/0.2.5'
+    download_url = 'https://github.com/sheriferson/simplestatistics/tarball/0.2.5',
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.txt').read(),
+    long_description=open('README.txt').read()
 )
