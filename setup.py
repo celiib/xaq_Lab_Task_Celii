50840c4bdc4c34429fd09d935027295e31804780
Brendan Celii <celiib@northampton.k12.nc.us>
2018-08-08 04:35:54 -0500

    fixing typos 1

--- setup.py
+++ setup.py
@@ -7,8 +7,9 @@ setup(
     author_email='celiibrendan@gmail.com',
     packages=['sfa'],
     install_requires=['torch','numpy','math','matplotlib'], #external packages as dependencies
-    url = 'https://github.com/sheriferson/simplestatistics',
-    download_url = 'https://github.com/sheriferson/simplestatistics/tarball/0.2.5',
+    setup_requires=['torch','numpy','math','matplotlib'],
+    url = 'https://github.com/celiib/xaq_Lab_Task_Celii',
+    download_url = 'https://github.com/celiib/xaq_Lab_Task_Celii.git',
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read()
 )
